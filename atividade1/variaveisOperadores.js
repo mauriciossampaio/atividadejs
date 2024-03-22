6edff41637c0283b24d8a381cef19e3b9920ac4e
mauriciossampaio <141499529+mauriciossampaio@users.noreply.github.com>
2024-03-22 10:05:04 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,24 @@
+let num_A = 9
+let num_B = 6
+let soma = num_A + num_B
+
+let num_A2 = 8
+let num_B2 = 5
+let sub = num_A2 - num_B2
+
+let num_A3 = 7
+let num_B3 = 4
+let div = num_A3 / num_B3
+
+let num_A4 = 6
+let num_B4 = 8
+let mult = num_A4 * num_B4
+
+let resultadosoma = "Resultado da soma ="
+let resultadosub = "Resultado da subtração ="
+let resultadodiv = "Resultado da divisão ="
+let resultadomult = "Resultado da multiplicação ="
+console.log(resultadosoma,soma);
+console.log(resultadosub,sub);
+console.log(resultadodiv,div);
+console.log(resultadomult,mult);
